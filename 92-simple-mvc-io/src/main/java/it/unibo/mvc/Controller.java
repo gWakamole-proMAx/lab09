0733a458e960f153ae769e1bdd90ea03591ee143
Sohaib Ouakani - sohaib.ouakani@studio.unibo.it <sohaib.ouakani@studio.unibo.it>
2024-11-18 12:50:22 +0100

    aggiunta la javadoc

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -14,6 +14,10 @@ public final class Controller {
 
     private File currentFile = new File(HOME + System.getProperty("file.separator") + DEFAULT_PATH);
 
+    /**
+     * Set the current file.
+     * @param newFile
+     */
     public void setCurrentFile(final File newFile) {
         final File parent = newFile.getParentFile();
         if (parent.exists()) {
@@ -23,14 +27,27 @@ public final class Controller {
         }
     }
 
+    /**
+     * Returns the current file.
+     * @return the current file
+     */
     public File getCurrentFile() {
         return this.currentFile;
     }
 
+    /**
+     * Retruns the current file path.
+     * @return the current file path
+     */
     public String getCurrentFilePath() {
         return this.currentFile.getPath();
     }
 
+    /**
+     * Writes the String paramter into the current file.
+     * @param line
+     * @throws IOException
+     */
     public void writeOnFile(final String line) throws IOException {
         try (PrintStream out = new PrintStream(this.currentFile, StandardCharsets.UTF_8)) {
             out.println(line);
