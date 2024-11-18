b1122e213b891c783d56f4522a4962715af0477a
Sohaib Ouakani - sohaib.ouakani@studio.unibo.it <sohaib.ouakani@studio.unibo.it>
2024-11-18 12:19:44 +0100

    fatto punto 1 esercizio 92, mancano le javadoc

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,39 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
-public class Controller {
+public final class Controller {
+    private static final String HOME = System.getProperty("user.home");
+    private static final String DEFAULT_PATH = "output.txt";
 
+    private File currentFile = new File(HOME + System.getProperty("file.separator") + DEFAULT_PATH);
+
+    public void setCurrentFile(final File newFile) {
+        final File parent = newFile.getParentFile();
+        if (parent.exists()) {
+            currentFile = newFile;
+        } else {
+            throw new IllegalArgumentException("folder does not exist");
+        }
+    }
+
+    public File getCurrentFile() {
+        return this.currentFile;
+    }
+
+    public String getCurrentFilePath() {
+        return this.currentFile.getPath();
+    }
+
+    public void writeOnFile(final String line) throws IOException {
+        try (PrintStream out = new PrintStream(this.currentFile, StandardCharsets.UTF_8)) {
+            out.println(line);
+        }
+    }
 }
