4cea3304812e00602b127fe7db964112ba1d7d2c
Sohaib Ouakani <sohaib.ouakani@studio.unibo.it>
2024-11-23 22:28:17 +0100

    realizzato interfaccia controller + implementazione

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,18 @@
 package it.unibo.mvc;
 
+import java.awt.List;
+import java.util.Collection;
+
 /**
  *
  */
 public interface Controller {
 
+    void setString(String newString);
+
+    String geString();
+
+    Collection<String> getHistory();
+
+    void printString();
 }
--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,43 @@
 package it.unibo.mvc;
 
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+    private String nexString;
+    final private Collection<String> historyString = new LinkedList<>();
+    
+    public SimpleController() {
+        this.nexString = null;
+    }
 
+    @Override
+    public String geString() {
+        return this.nexString;
+    }
+
+    @Override
+    public Collection<String> getHistory() {
+        return List.copyOf(historyString);
+    }
+
+    @Override
+    public void printString() {
+        if(Objects.nonNull(this.nexString)) {
+            System.out.println(this.nexString);
+        } else {
+            throw new IllegalStateException("the string is not set");
+        }
+    }
+
+    @Override
+    public void setString(String newString) {
+        this.nexString = Objects.requireNonNull(newString);
+    }
 }
