d5db2d81d10756c29135e916dd1e827f47c81f61
Sohaib Ouakani <sohaib.ouakani@studio.unibo.it>
2024-11-23 16:11:44 +0100

    finito di implementare le funzionalità

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -2,6 +2,8 @@ package it.unibo.mvc;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 /**
@@ -18,23 +20,57 @@ public final class SimpleGUIWithFileChooser {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         final JPanel canvas = new JPanel();
         canvas.setLayout(new BorderLayout());
-        final JTextField fileName = new JTextField("Choose File");
-        fileName.setEditable(false);
-        final JButton chooseFile = new JButton("Browse");
+        final JTextField pickedFile = new JTextField("Choose File");
+        pickedFile.setEditable(false);
+        final JButton save = new JButton("Save");
+
+        save.addActionListener(
+            new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    myController.writeOnFile(myController.getCurrentFilePath());
+                } catch (Exception e1) {
+                    JOptionPane.showMessageDialog(save, "An error has occured", "Error",0);
+                }
+            }
+        });
+
+        final JButton browse = new JButton("Browse");
+
+        browse.addActionListener(
+            new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    final JFileChooser fileChooser = new JFileChooser("Choose a file");
+                    if(fileChooser.showSaveDialog(browse) == JFileChooser.APPROVE_OPTION) {
+                        myController.setCurrentFile(fileChooser.getSelectedFile());
+                        pickedFile.setText(myController.getCurrentFilePath());
+                        return;
+                    } else if(fileChooser.showSaveDialog(browse) == JFileChooser.CANCEL_OPTION) {
+                        return;
+                    } else {
+                        JOptionPane.showMessageDialog(browse, "An error has occured", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            });
 
         final JPanel fileChoosingArea = new JPanel();
         fileChoosingArea.setLayout(new BorderLayout());
 
         frame.add(canvas);
         canvas.add(fileChoosingArea, BorderLayout.NORTH);
-        fileChoosingArea.add(fileName, BorderLayout.CENTER);
-        fileChoosingArea.add(chooseFile, BorderLayout.LINE_END);
+        canvas.add(save, BorderLayout.SOUTH);
+        fileChoosingArea.add(pickedFile, BorderLayout.CENTER);
+        fileChoosingArea.add(browse, BorderLayout.LINE_END);
 
         final Dimension screeDimension = Toolkit.getDefaultToolkit().getScreenSize();
         final int width = (int)screeDimension.getWidth()/5;
         final int height = (int)screeDimension.getHeight()/5;
 
         frame.setSize(width, height);
+        frame.setLocationByPlatform(true);
     }
 
     public void display() {
